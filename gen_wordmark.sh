a3d84f01410b1a021aacdc18b4c4f337dbd3b346
wannaskoo <wannaskoo@tutanota.com>
2021-04-18 17:39:20 +0200

    gen_wordmark.sh: edit to reflect name change

--- gen_wordmark.sh
+++ gen_wordmark.sh
@@ -2,8 +2,8 @@
 #License: AGPL-3.0
 
 drawable="./fenix-overlay/res/drawable";
-font="Fira-Sans-Bold";
-wordmark="Mull";
+font="Iosevka-Bold";
+wordmark="fennec_dos";
 
 convert -background transparent -fill black -font $font -gravity center -size x80 label:$wordmark $drawable-mdpi/ic_logo_wordmark_normal.png
 convert -background transparent -fill white -font $font -gravity center -size x80 label:$wordmark $drawable-mdpi/ic_logo_wordmark_private.png
